bcc806292088564fbcba6b471c7f2b974923d7c9
Vitalii Kurhanskyi <vitalii.kurhanskyi@gmail.com>
2025-08-30 22:39:01 +0200

    Lesson 13 about data filter is completed

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Worker\IndexRequest;
 use App\Http\Requests\Worker\StoreRequest;
 use App\Http\Requests\Worker\UpdateRequest;
 use App\Models\Worker;
@@ -9,40 +10,79 @@ use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    public function index() {
-        $workers = Worker::all();
-        //dd($workers);
-        // foreach($workers as $worker) {
-        //     dump($worker->name);
-        // }
+    public function index(IndexRequest $filter) 
+    {
+        $data = $filter->validated();
+        $workerQuery = Worker::query();
+
+        if(isset($data['name'])) {
+            $workerQuery->where('name', 'like', "%{$data['name']}%");
+        }
+
+        if(isset($data['surname'])) {
+            $workerQuery->where('surname', 'like', "%{$data['surname']}%");
+        }
+
+        if(isset($data['email'])) {
+            $workerQuery->where('email', 'like', "%{$data['email']}%");
+        }
+
+        if(isset($data['from'])) {
+            $workerQuery->where('age', '>', $data['from']);
+        }
+
+        if(isset($data['to'])) {
+            $workerQuery->where('age', '<', $data['to']);
+        }
+
+        if(isset($data['description'])) {
+            $workerQuery->where('description', 'like', "%{$data['description']}%");
+        }
+
+        if(isset($data['is_married'])) {
+            $workerQuery->where('is_married', '=', $data['is_married']);
+            //dump($data['is_married']);
+        }
+
+        
+
+        $workers = $workerQuery->paginate(4);
+
+        //$workers = Worker::paginate(4);
         return view('worker.index', compact('workers'));
     }
 
-    public function show(Worker $worker) {
+    public function show(Worker $worker)
+    {
         return view('worker.show', compact('worker'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('worker.create');
     }
 
-    public function store(StoreRequest $request) {
+    public function store(StoreRequest $request) 
+    {
         $data = $request->validated();
         Worker::create($data);
         return redirect()->route('workers.index');
     }
 
-    public function edit(Worker $worker) {
+    public function edit(Worker $worker) 
+    {
         return view('worker.edit', compact('worker'));
     }
 
-    public function update(UpdateRequest $request, Worker $worker) {
+    public function update(UpdateRequest $request, Worker $worker) 
+    {
         $data = $request->validated();
         $worker->update($data);
         return redirect()->route('workers.show', $worker->id);
     }
 
-    public function delete($id) {
+    public function delete($id) 
+    {
         $worker = Worker::findOrFail($id);
         $worker->delete();
         return redirect()->route('workers.index');
--- resources/views/worker/index.blade.php
+++ resources/views/worker/index.blade.php
@@ -4,10 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Index Workers Page</title>
 </head>
 <body>
     <h1>Index Workers Page</h1>
+    <div class="filter">
+        <form action="{{ route('workers.index') }}" mathod="GET">
+            <input type="text" name="name" placeholder="name" value="{{ request()->get('name') }}">
+            <input type="text" name="surename" placeholder="surename" value="{{ request()->get('surname') }}">
+            <input type="text" name="email" placeholder="email" value="{{ request()->get('email') }}">
+            <input type="number" name="from" placeholder="from" value="{{ request()->get('from') }}">
+            <input type="number" name="to" placeholder="to" value="{{ request()->get('to') }}">
+            <input type="text" name="description" placeholder="description">
+            <br>
+            <label for="is_married">Is married:</label>            
+            <input id="is_married" type="checkbox" name="is_married" value="1" {{ request()->get('is_married') ? 'checked' : '' }}>
+            <input type="submit" value="Seach">
+            <a href="{{ route('workers.index') }}">Reset Filter</a>
+        </form>
+    </div>
     <div>
         <hr>
         <a href="{{ route('workers.create') }}">Create New Worker</a>
@@ -24,6 +40,8 @@
                     <p>Worker name: {{$worker->name}}</p>
                     <p>Worker lastname: {{$worker->surname}}</p>
                     <p>Worker age: {{ $worker->age }} </p>
+                    <p>Worker description: {{ $worker->description }}</p>
+                    <p>Is married: {{ $worker->is_married ? "married" : "not married" }}</p>
                     <a href="{{ route('workers.show', $worker->id) }}">Show The Worker Info</a>
                     <br>
                     <a href="{{ route('workers.edit', $worker->id) }}">Edit the Worker Info</a>
@@ -32,5 +50,8 @@
             @endforeach 
         </ul>       
     @endif
+    <div class="my-nav">
+        {{ $workers->links() }}
+    </div>
 </body>
 </html>
